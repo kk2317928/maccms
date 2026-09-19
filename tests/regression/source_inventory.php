04e87e7d9e8d5304615ad0f7a3a11783e6b3fcdf
nothing <afr20170615@gmail.com>
2026-09-20 04:03:43 +0800

    test: reconcile T-072 utility inventory

--- tests/regression/source_inventory.php
+++ tests/regression/source_inventory.php
@@ -59,7 +59,7 @@ $expectedCounts = [
     'api_controllers' => 39,
     'index_controllers' => 24,
     'common_models' => 68,
-    'common_utilities' => 117,
+    'common_utilities' => 119,
 ];
 
 foreach ($expectedCounts as $name => $expected) {
