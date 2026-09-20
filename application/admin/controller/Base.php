4a90d5f280ca1be167ac783213a351e6bf94c733
nothing <afr20170615@gmail.com>
2026-09-20 18:42:44 +0900

    T-090 remove update controller bypass and integrity gate

--- application/admin/controller/Base.php
+++ application/admin/controller/Base.php
@@ -18,15 +18,6 @@ class Base extends All
     {
         parent::__construct();
 
-        // 校验Update.php文件完整性；放行 Update 控制器自身，避免 hash 失配时升级 UI 也被锁死
-        if ($this->_cl != 'Update') {
-            $update_file = APP_PATH . 'admin/controller/Update.php';
-            $expected_hash = config('version.update_hash');
-            if (!empty($expected_hash) && is_file($update_file) && md5_file($update_file) !== $expected_hash) {
-                exit(lang('admin/update/core_file_error'));
-            }
-        }
-
         //判断用户登录状态
         if(in_array($this->_cl,['Index']) && in_array($this->_ac,['login'])) {
 
@@ -43,7 +34,7 @@ class Base extends All
             $this->_pagesize = $GLOBALS['config']['app']['pagesize'];
             $this->_makesize = $GLOBALS['config']['app']['makesize'];
 
-            if($this->_cl!='Update' && !$this->check_auth($this->_cl,$this->_ac)){
+            if(!$this->check_auth($this->_cl,$this->_ac)){
                 return $this->error(lang('permission_denied'));
             }
         }
