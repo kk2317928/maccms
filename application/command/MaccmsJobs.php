4dd6e56d3144b6453b07cba9ceede176f02914d5
nothing <afr20170615@gmail.com>
2026-09-21 01:22:40 +0900

    fix: wire installed AI worker by default

--- application/command/MaccmsJobs.php
+++ application/command/MaccmsJobs.php
@@ -5,6 +5,14 @@ namespace app\command;
 use app\common\util\ContentJobRepository;
 use app\common\util\ContentJobWorker;
 use app\common\util\TmdbReviewJobHandler;
+use app\common\util\AiNormalizationJobHandler;
+use app\common\util\AiNormalizationPipeline;
+use app\common\util\AiNormalizationProvider;
+use app\common\util\AiNormalizationValidator;
+use app\common\util\AiProvider;
+use app\common\util\AiRunRepository;
+use app\common\util\FieldGovernance;
+use app\common\util\DuplicateCandidateDetector;
 use think\console\Command;
 use think\console\Input;
 use think\console\input\Option;
@@ -28,6 +36,24 @@ class MaccmsJobs extends Command
         $aiHandler = config('maccms.ai_normalization_handler');
         if (is_callable($aiHandler)) {
             $handlers['ai.normalize'] = $aiHandler;
+        } else {
+            $aiConfig = AiProvider::resolveConfig();
+            $pipeline = new AiNormalizationPipeline(
+                new AiNormalizationProvider($aiConfig),
+                new AiNormalizationValidator(),
+                new AiRunRepository(),
+                new FieldGovernance(),
+                [
+                    'provider' => (string) $aiConfig['provider'],
+                    'model' => (string) $aiConfig['model'],
+                    'prompt_version' => 'normalize-v1',
+                    'daily_budget_micros' => (int) $aiConfig['daily_budget_micros'],
+                    'auto_adopt_empty' => !empty($aiConfig['auto_adopt_empty']),
+                ],
+                null,
+                new DuplicateCandidateDetector()
+            );
+            $handlers['ai.normalize'] = new AiNormalizationJobHandler($pipeline);
         }
         $tmdbHandler = new TmdbReviewJobHandler();
         $handlers['tmdb_match'] = [$tmdbHandler, 'match'];
