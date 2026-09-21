de1f0c3984eec1aa3ad0bde0e0e58257c52fb9a4
nothing <afr20170615@gmail.com>
2026-09-22 05:46:45 +0900

    test: account for taxonomy dictionary service

--- tests/regression/source_inventory.php
+++ tests/regression/source_inventory.php
@@ -59,7 +59,7 @@ $expectedCounts = [
     'api_controllers' => 39,
     'index_controllers' => 24,
     'common_models' => 68,
-    'common_utilities' => 153,
+    'common_utilities' => 154,
 ];
 
 foreach ($expectedCounts as $name => $expected) {
