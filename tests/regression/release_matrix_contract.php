35c36277dc751e29c801389064109d7ed563da1a
nothing <afr20170615@gmail.com>
2026-09-22 19:25:31 +0900

    test(T-116): update release migration count contract

--- tests/regression/release_matrix_contract.php
+++ tests/regression/release_matrix_contract.php
@@ -24,7 +24,7 @@ $required = [
     "php tests/integration/taxonomy_review_mysql.php",
     "php tests/integration/native_vod_paths.php",
     "php tests/regression/outbound_inventory.php --enforce",
-    "done, applied=0, skipped=21",
+    "done, applied=0, skipped=22",
 ];
 foreach ($required as $needle) {
     if (strpos($source, $needle) === false) {
