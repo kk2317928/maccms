7288904ce581873222550ef1348362d3847bf9f8
nothing <afr20170615@gmail.com>
2026-09-22 17:55:46 +0900

    test(T-114): add poster ingestion to TMDB suite

--- tests/regression/run_duplicate_tmdb.php
+++ tests/regression/run_duplicate_tmdb.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-$tests = ['ai_usage_and_duplicate_config.php', 'duplicate_relational_merge.php', 'content_workflow_handoff.php', 'duplicate_tmdb_suite_contract.php', 'duplicate_candidate_scoring.php', 'duplicate_candidate_decisions.php', 'duplicate_merge.php', 'canonical_public_id.php', 'duplicate_restore.php', 'tmdb_matching.php', 'tmdb_import.php'];
+$tests = ['ai_usage_and_duplicate_config.php', 'duplicate_relational_merge.php', 'content_workflow_handoff.php', 'duplicate_tmdb_suite_contract.php', 'duplicate_candidate_scoring.php', 'duplicate_candidate_decisions.php', 'duplicate_merge.php', 'canonical_public_id.php', 'duplicate_restore.php', 'tmdb_matching.php', 'tmdb_import.php', 'external_image_ingestion.php', 'tmdb_review_ui.php'];
 foreach ($tests as $test) {
     fwrite(STDOUT, "RUN: {$test}\n");
     passthru(escapeshellarg(PHP_BINARY) . ' ' . escapeshellarg(__DIR__ . '/' . $test), $exitCode);
